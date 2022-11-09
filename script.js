7ca504092f7ff0276976aeada3ad0ee984de2e94
Matt Brooks <115656591+mattbrooks100@users.noreply.github.com>
2022-11-09 18:51:09 -0500

    Regular checkerboard finished

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+for (let i = 0; i < 81; i++) {
+  if (i % 2 === 1) {
+    const div = document.createElement("div");
+    document.body.append(div);
+    div.style.width = "11.1%";
+    div.style.float = "left";
+    div.style.paddingBottom = "11.1%";
+    div.style.background = "red";
+  } else {
+    const div = document.createElement("div");
+    document.body.append(div);
+    div.style.width = "11.1%";
+    div.style.float = "left";
+    div.style.paddingBottom = "11.1%";
+    div.style.background = "black";
+  }
+}
